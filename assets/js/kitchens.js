bb881690e61821fce2cea559f48febb5c4047873
Ethan <spaminol@gmail.com>
2021-05-18 13:11:56 -0700

    Fix lightbox

--- assets/js/kitchens.js
+++ assets/js/kitchens.js
@@ -1,4 +1,6 @@
 $('.kitchen-image').magnificPopup({
   type: 'image',
-  src: 'https://www.finearttilestudio.com/assets/images/IMG_1182-GailLysterKitchenArtCR.jpg'
+  items: {
+    src: 'https://www.finearttilestudio.com/assets/images/IMG_1182-GailLysterKitchenArtCR.jpg'
+  }
 });
